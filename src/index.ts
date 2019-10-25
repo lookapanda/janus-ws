aa4dbd523fe6ba1787fc63d50320caff7f550484
Simon <sn@hitbox.tv>
2019-10-25 15:33:49 +0200

    fix: fix class export :(

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './janus-ws-plugin';
 export * from './message-types';
 export * from './janus-ws-config';
 export * from './plugins';
+// for some reaseon we need to explicitly export the class, otherwise no types are exported for it :(
+export { JanusWsStreamingPlugin } from './plugins';
